17b45e9a6ff91c633930a0e81f5f3bcc86280b29
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-06-16 13:27:42 +0200

    Add store and store dispatch

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,3 +1,11 @@
-console.log("Hello World!");
+import store from "./store";
 
+const unsubscribe = store.subscribe(()=>{
+    console.log("Store changed!", store.getState())
+})
 
+store.dispatch({type: "bugAdded", payload:{description: "Bug 1"}})
+unsubscribe()
+store.dispatch({type:"bugRemoved", payload:{id: 1}})
+
+console.log(store.getState())
