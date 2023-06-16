7ec5c68406e3f2d4ca724c7e011e5d5d22b6365e
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-06-16 19:15:27 +0200

    Add index.js with initial state, reducer and actions

---
+++ redux-demo/index.js
@@ -0,0 +1,77 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+// import {createStore} from "redux"
+// import store from "./store"
+
+const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
+
+const initialState = {
+  numberOfCakes: 10,
+  numberOfIceCreams: 20
+};
+
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    payload: 1,
+  };
+}
+function restockedCake(qty) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
+  };
+}
+function orderIce(qty ) {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+}
+function restockedIce(qty =2) {
+    return {
+      type: ICECREAM_RESTOCKED,
+      payload: qty,
+    };
+  }
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes - 1,
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes + action.payload,
+      };
+      case ICECREAM_ORDERED:
+        return {
+          ...state,
+          numberOfIceCreams: state.numberOfIceCreams - 1,
+        };
+      case ICECREAM_RESTOCKED:
+        return {
+          ...state,
+          numberOfIceCreams: state.numberOfIceCreams + action.payload,
+        };
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+console.log("InitialState", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated state", store.getState())
+);
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(restockedCake(8));
+store.dispatch(orderIce())
+store.dispatch(restockedIce(6))
+unsubscribe();
