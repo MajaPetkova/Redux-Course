43ae4c6985da1097160bdab583f14372e8c17dfc
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-06-19 09:26:52 +0200

    Added middleware and redux-logger library

--- rdk-demo/index.js
+++ rdk-demo/index.js
@@ -5,7 +5,7 @@ const iceCreamActions = require("./features/iceCream/iceCreamSlice").iceCreamAct
 
 console.log("Initial state", store.getState())
 const unsubscribe= store.subscribe(()=>{
-    console.log("Updated State", store.getState())
+    // console.log("Updated State", store.getState())
 })
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.restocked(10))
