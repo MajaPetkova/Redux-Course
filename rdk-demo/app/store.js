4a29081bbfe44b25b4738845e532a14e28aad9fd
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-06-19 11:20:12 +0200

    create userSlice - managing asynchronous request lifecycles

--- rdk-demo/app/store.js
+++ rdk-demo/app/store.js
@@ -2,14 +2,15 @@ const configureStore= require("@reduxjs/toolkit").configureStore;
 // const reduxLogger = require("redux-logger")
 const cakeReducer = require("../features/cake/cakeSlice");
 const iceCreamReducer = require ("../features/iceCream/iceCreamSlice");
-
+const userReducer = require("../features/user/userSlice")
 
 // const logger= reduxLogger.createLogger();
 
 const store= configureStore({
    reducer: {
     cake:cakeReducer,
-    iceCream: iceCreamReducer
+    iceCream: iceCreamReducer,
+    users: userReducer
    },
 //    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
--- rdk-demo/index.js
+++ rdk-demo/index.js
@@ -1,18 +1,20 @@
 const store = require("./app/store");
 const cakeActions =require("./features/cake/cakeSlice").cakeActions;
 const iceCreamActions = require("./features/iceCream/iceCreamSlice").iceCreamActions;
-
+const fetchUsers = require("./features/user/userSlice").fetchUsers
 
 console.log("Initial state", store.getState())
 const unsubscribe= store.subscribe(()=>{
     console.log("Updated State", store.getState())
 })
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.restocked(10))
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
 
-store.dispatch(iceCreamActions.ordered())
-store.dispatch(iceCreamActions.restocked(4))
+store.dispatch(fetchUsers())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restocked(10))
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
 
-unsubscribe()
+// store.dispatch(iceCreamActions.ordered())
+// store.dispatch(iceCreamActions.restocked(4))
+
+// unsubscribe()
