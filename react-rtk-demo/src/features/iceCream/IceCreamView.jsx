2954edc4cd0bcbfb76ffe5d55eacb35bd65e8a59
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-06-20 17:19:08 +0200

    Initialize react app and add components

---
+++ react-rtk-demo/src/features/iceCream/IceCreamView.jsx
@@ -0,0 +1,10 @@
+export const IceCreamView =()=>{
+    return (
+        <div>
+            <h2>Number of IceCreams: </h2>
+            <button>Order IceCream</button>
+            <button>Restock CIceCream</button>
+        </div>
+
+    )
+}
---
+++ react-rtk-demo/src/features/user/UserView.jsx
@@ -0,0 +1,7 @@
+export const UserView= ()=>{
+    return (
+        <div>
+            <h3>List of Users</h3>
+        </div>
+    )
+}
