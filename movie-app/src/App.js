a44d1355a7a76814b23714a33cdbf6399e5b2061
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-06-21 09:59:05 +0200

    Update header component and add scss and routing

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,8 +1,27 @@
 import "./App.scss";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./components/home/Home";
+import {Header} from "./components/header/Header";
+import { MovieDetails } from "./components/movieDetails/MovieDetails";
+import { PageNotFound } from "./components/pageNotFound/PageNotFound";
+import { Footer } from "./components/footer/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Header></Header>
+        <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/movie/:id" element={<MovieDetails/>} />
+          <Route path="/*" element={<PageNotFound/>} />
+        </Routes>
+        </div>
+        <Footer/>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- movie-app/src/components/header/Header.js
+++ movie-app/src/components/header/Header.js
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+import user from "../../images/user.png"
+import "./header.scss";
+
+
 export const Header=()=>{
     return(
-        <div>
-            Header
-        </div>
+        <div className="header">
+        <Link to="/">
+      <div className="logo">
+        Movie App
+      </div>
+        </Link>
+      <div className="user-image">
+        <img src={user} alt="user-img" className="user-img" />
+      </div>
+    </div>
     )
 }
