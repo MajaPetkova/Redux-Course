56c88a3ec906bb04bb203a3c222fb8f6aa809ccc
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-06-21 11:25:41 +0200

    Add slice and store from redux-toolkit, update footer and header components

--- movie-app/src/components/footer/Footer.js
+++ movie-app/src/components/footer/Footer.js
@@ -1,7 +1,10 @@
-export const Footer =() =>{
-    return(
-        <div>
-            footer
-        </div>
-    )
-}
+import "./footer.scss";
+
+export const Footer = () => {
+  return (
+    <div className="footer-container">
+        <div>Movie App</div>
+        <div>&copy;2023, Movie - M.P </div>
+    </div>
+  );
+};
--- movie-app/src/components/home/Home.js
+++ movie-app/src/components/home/Home.js
@@ -1,5 +1,26 @@
-export const Home = () =>{
-    return(
-        <div></div>
-    )
-}
+import { MovieListing } from "../movieListing/MovieListing";
+import { useEffect } from "react";
+import movieApi from "../../common/apis/MovieApi";
+import { APIKey } from "../../common/apis/MovieApiKey";
+
+export const Home = () => {
+  const movieText = "Harry";
+  const fetchMovies = async () => {
+    const response = await movieApi
+      .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
+};
